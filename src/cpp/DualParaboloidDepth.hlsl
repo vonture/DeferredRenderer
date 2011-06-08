62fab019dfe07b0cecb7d1970e767290fa29f02f
Geoff Lang <geoff@vonture.net>
2011-06-07 22:32:46 -0400

    Formatted the comments slightly.

--- src/cpp/DualParaboloidDepth.hlsl
+++ src/cpp/DualParaboloidDepth.hlsl
@@ -28,14 +28,15 @@ VS_Out_Depth VS_Depth(VS_In_Depth input)
 	output.vPositionCS.z = output.vPositionCS.z * Direction;
 	
 	float L = length(output.vPositionCS.xyz);							// determine the distance between (0,0,0) and the vertex
-	output.vPositionCS = output.vPositionCS / L;								// divide the vertex position by the distance 
+	output.vPositionCS = output.vPositionCS / L;						// divide the vertex position by the distance 
 
-	output.vPositionCS.z = output.vPositionCS.z + 1.0f;							// add the reflected vector to find the normal vector
+	output.vPositionCS.z = output.vPositionCS.z + 1.0f;					// add the reflected vector to find the normal vector
 
-	output.vPositionCS.x = output.vPositionCS.x / output.vPositionCS.z;					// divide x coord by the new z-value
-	output.vPositionCS.y = output.vPositionCS.y / output.vPositionCS.z;					// divide y coord by the new z-value
+	output.vPositionCS.x = output.vPositionCS.x / output.vPositionCS.z;	// divide x coord by the new z-value
+	output.vPositionCS.y = output.vPositionCS.y / output.vPositionCS.z;	// divide y coord by the new z-value
 
-	output.vPositionCS.z = (L - CameraClips.x) / (CameraClips.y - CameraClips.x);	// scale the depth to [0, 1]
+	output.vPositionCS.z = (L - CameraClips.x) /						// scale the depth to [0, 1]
+						   (CameraClips.y - CameraClips.x);	
 	output.vPositionCS.w = 1;											// set w to 1 so there is no w divide
 	
     return output;
