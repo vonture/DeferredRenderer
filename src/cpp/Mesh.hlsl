419484f3f24633e6d7bd71ad0552ea4a43928f18
Geoff Lang <geoff@vonture.net>
2011-06-09 17:14:47 -0400

    Slightly fixed the pack offsets.

--- src/cpp/Mesh.hlsl
+++ src/cpp/Mesh.hlsl
@@ -1,8 +1,21 @@
+static const float3 Grey = float3(0.212671f, 0.715160f, 0.072169f); 
+
 cbuffer cbModelProperties : register(b0)
 {
-    float4x4 World					: packoffset(c0);
-    float4x4 WorldViewProjection	: packoffset(c4);
+    float4x4 World					: packoffset(c0.x);
+    float4x4 WorldViewProjection	: packoffset(c4.x);
 }
+
+cbuffer cbMaterialProperties : register(b1)
+{
+	float3 AmbientColor		: packoffset(c0);
+    float3 DiffuseColor		: packoffset(c1);
+    float3 EmissiveColor	: packoffset(c2);    
+	float3 SpecularColor	: packoffset(c3);
+	float SpecularPower		: packoffset(c4.x);
+    float Alpha				: packoffset(c4.y);
+	float2 Padding			: packoffset(c4.z);
+};
 
 Texture2D DiffuseMap	: register(t0);
 Texture2D NormalMap		: register(t1);
@@ -59,14 +72,22 @@ PS_Out_Mesh PS_Mesh(VS_Out_Mesh input)
 	float3 vNormalTS = (NormalMap.Sample(Sampler, input.vTexCoord) * 2.0f) - 1.0f;
 	float3 vNormal = normalize(mul(vNormalTS, mTangentToWorld));
 
-	float4 vDiffuse = DiffuseMap.Sample(Sampler, input.vTexCoord);		
+	float4 vDiffuse = DiffuseMap.Sample(Sampler, input.vTexCoord);
 
-    // RT0 =       Diffuse.r           | Diffuse.g         | Diffuse.b     | Specular Intensity
-    // RT1 =       Normal.x            | Normal.y          | Normal.z      | Specular Power
-    // RT2 =                           | Ambient Occlusion | Translucency  | Material ID
-	output.RT0 = float4(vDiffuse.rgb, 0.0f);
-	output.RT1 = float4(vNormal, 1.0f);
-	output.RT2 = float4(0.0f, 0.02f, 0.0f, 0.0f);
+	float3 vEmissive = EmissiveColor;
+	float fSpecularIntensity = vDiffuse.a;
+	float fSpecularPower = SpecularPower;
+
+	float vAmbientColor = AmbientColor * Grey;
+	float fAmbient = AmbientColor.r + AmbientColor.g + AmbientColor.b;
+
+    // RT0 =       Diffuse.r	| Diffuse.g		| Diffuse.b		| Specular Intensity
+    // RT1 =       Normal.x		| Normal.y		| Normal.z		| Specular Power
+    // RT2 =       Emissive.r	| Emissive.g	| Emissive.b	| Ambient
+    // RT3 =       Depth		|				|				|
+	output.RT0 = float4(vDiffuse.rgb, fSpecularIntensity);
+	output.RT1 = float4(vNormal, fSpecularPower);
+	output.RT2 = float4(vEmissive, fAmbient);
 
 	return output;
 }
