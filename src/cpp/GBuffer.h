d036ee6c50f97117bf6256dc632e2da90b4c03d2
Geoff Lang <geoff@vonture.net>
2011-06-15 21:18:32 -0400

    Modified the contents of the gbuffer, no longer stores ambient color.

--- src/cpp/GBuffer.h
+++ src/cpp/GBuffer.h
@@ -6,9 +6,9 @@
 class GBuffer : public DeferredBuffer
 {
 private:
-    // RT0 =       Diffuse.r	| Diffuse.g		| Diffuse.b		| Specular Intensity
+	// RT0 =       Diffuse.r	| Diffuse.g		| Diffuse.b		| Specular Intensity
     // RT1 =       Normal.x		| Normal.y		| Normal.z		| Specular Power
-    // RT2 =       Emissive.r	| Emissive.g	| Emissive.b	| Ambient
+    // RT2 =       Emissive.r	| Emissive.g	| Emissive.b	| Material ID
     // RT3 =       Depth		|				|				|
 	ID3D11Texture2D* _textures[4];
 	ID3D11ShaderResourceView* _shaderResourceViews[4];
