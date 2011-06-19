5271468a3d6a4d03a0b273beef05665c4f16d423
Geoff Lang <geoff@vonture.net>
2011-06-19 16:12:31 -0400

    Defines now links all the directx libs and added some comments.

--- src/cpp/Defines.h
+++ src/cpp/Defines.h
@@ -1,17 +1,37 @@
 #pragma once
 
-#pragma warning(disable: 4324)
+// Namespace usings
+using namespace std;
 
 #include "DXUT.h"
 
-//#define _XM_NO_INTRINSICS_
-//#include <xnamath.h>
-
+// XNA Math and collision
+#include <xnamath.h>
 #include "xnaCollision.h"
 
-// Some xna collision defines
-#define NO_INTERSECTION 0
-#define INTERSECTION 1
-#define COMPLETELY_INSIDE 2
-
+// Some utility functions
 #include "Utility.h"
+
+// Static Lib Imports
+#pragma comment(lib, "dxerr.lib")
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "D3D9.lib")
+#pragma comment(lib, "D3D11.lib")
+#pragma comment(lib, "D3DX10.lib")
+#pragma comment(lib, "D3DX11.lib")
+#pragma comment(lib, "DXGI.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "psapi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "gdiplus.lib")
+
+#ifdef _DEBUG
+#pragma comment(lib, "comsuppwd.lib")
+#pragma comment(lib, "d3dx9d.lib")
+#else
+#pragma comment(lib, "comsuppw.lib")
+#pragma comment(lib, "d3dx9.lib")
+#endif
+
+// Warning disables
+#pragma warning(disable: 4324) // Padding added from aligned members
