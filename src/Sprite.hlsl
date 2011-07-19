f875c14a97310235094c050f85193018b95e5f76
Geoff Lang <geoff@vonture.net>
2011-07-18 22:46:00 -0400

    Added a sprite shader.

---
+++ src/Sprite.hlsl
@@ -0,0 +1,34 @@
+struct VS_In_Sprite
+{
+    float4 vPosition : POSITION;
+    float2 vTexCoord : TEXCOORD0;
+	float4 vColor	 : COLOR0;
+};
+
+struct VS_Out_Sprite
+{
+	float4 vPosition : SV_POSITION;
+	float2 vTexCoord : TEXCOORD0;
+	float4 vColor	 : COLOR0;
+};
+
+Texture2D SpriteTexture : register(t0);
+SamplerState SpriteSampler : register(s0);
+
+VS_Out_Sprite VS_Sprite(VS_In_Sprite input)
+{
+    VS_Out_Sprite output;
+
+    output.vPosition = input.vPosition;
+    output.vTexCoord = input.vTexCoord;
+	output.vColor = input.vColor;
+
+    return output;
+}
+
+float4 PS_Sprite(VS_Out_Sprite input) : SV_TARGET0
+{
+	float4 vFontColor = SpriteTexture.SampleLevel(SpriteSampler, input.vTexCoord, 0);
+	
+	return vFontColor * input.vColor;
+}
