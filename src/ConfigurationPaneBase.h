5e0e459b7b228c284a056208a6a209bf2808d8a3
Geoff Lang <geoff@vonture.net>
2011-07-20 01:36:44 -0400

    Configuration panes are no scroll controls instead of group boxes.

--- src/ConfigurationPaneBase.h
+++ src/ConfigurationPaneBase.h
@@ -2,12 +2,17 @@
 
 #include "Defines.h"
 #include "IUpdateable.h"
-#include "Gwen/Controls/GroupBox.h"
+#include "Gwen/Controls/ScrollControl.h"
 
-class ConfigurationPaneBase : public IUpdateable, public Gwen::Controls::GroupBox
+class ConfigurationPaneBase : public IUpdateable, public Gwen::Controls::ScrollControl
 {
 public:
-	ConfigurationPaneBase(Gwen::Controls::Base* parent) : Gwen::Controls::GroupBox(parent) { }
+	ConfigurationPaneBase(Gwen::Controls::Base* parent) 
+		: Gwen::Controls::ScrollControl(parent) 
+	{ 
+		SetScroll(false, true);
+		SetAutoHideBars(true);
+	}
 	
 	virtual void OnFrameMove(double totalTime, float dt) = 0;
 };
