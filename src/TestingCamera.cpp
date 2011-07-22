1c66432f6caf048e9cf9349d915573a147880819
Geoff Lang <geoff@vonture.net>
2011-07-21 22:43:59 -0400

    Added a new camera class that has properties such as movement and rotation speed that can be configured.

---
+++ src/TestingCamera.cpp
@@ -0,0 +1,31 @@
+#include "TestingCamera.h"
+
+TestingCamera::TestingCamera()
+	: FirstPersonCamera(), _moveSpeed(10.0f), _rotSpeed(0.002f)
+{
+}
+
+TestingCamera::TestingCamera(float nearClip, float farClip, float fov, float aspect)
+	: FirstPersonCamera(nearClip, farClip, fov, aspect), _moveSpeed(10.0f), _rotSpeed(0.002f)
+{
+}
+
+float TestingCamera::GetMovementSpeed() const 
+{ 
+	return _moveSpeed;
+}
+
+void TestingCamera::SetMovementSpeed(float speed)
+{ 
+	_moveSpeed = max(0.0f, speed); 
+}
+
+float TestingCamera::GetRotationSpeed() const
+{
+	return _rotSpeed;
+}
+
+void TestingCamera::SetRotationSpeed(float rotSpeed)
+{
+	_rotSpeed = max(0.0f, rotSpeed);
+}
---
+++ src/TestingCamera.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Defines.h"
+#include "FirstPersonCamera.h"
+
+class TestingCamera : public FirstPersonCamera
+{
+private:
+	float _moveSpeed;
+	float _rotSpeed;
+
+public:
+	TestingCamera();
+	TestingCamera(float nearClip, float farClip, float fov, float aspect);
+
+	float GetMovementSpeed() const;
+	void SetMovementSpeed(float speed);
+
+	float GetRotationSpeed() const;
+	void SetRotationSpeed(float rotSpeed);
+};
