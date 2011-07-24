64c74a476f176c286d92dea9f87debbb76f1461f
Geoff Lang <geoff@vonture.net>
2011-07-24 16:12:20 -0400

    Added an epsilon define to standardize a little bit.

--- src/Utility.h
+++ src/Utility.h
@@ -5,6 +5,8 @@ const float PiOver2 = Pi / 2.0f;
 const float PiOver4 = Pi / 4.0f;
 const float PiOver8 = Pi / 8.0f;
 
+#define EPSILON 0.00001f
+
 #ifndef clamp
 #define clamp(val, min, max) (((val) < (min)) ? (min) : (((val) > (max)) ? (max) : (val)))
 #endif
