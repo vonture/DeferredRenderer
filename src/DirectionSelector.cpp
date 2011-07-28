768118f6241db2b8dc93b449d9ea02b3b895fe62
Geoff Lang <geoff@vonture.net>
2011-07-28 14:03:01 -0400

    Direction selector now returns vectors of length 1 - EPSILON so that when compu- ting a 3d vector, there will be no sqrt(0).

--- src/DirectionSelector.cpp
+++ src/DirectionSelector.cpp
@@ -32,11 +32,11 @@ void DirectionSelector::PlaceInnerCircle()
 
 void DirectionSelector::VerifyDirection()
 {
-	if (_direction.x * _direction.x + _direction.y * _direction.y > 1.0f ||
+	if (_direction.x * _direction.x + _direction.y * _direction.y >= 1.0f ||
 		(_direction.x == 0.0f && _direction.y == 0.0f))
 	{
 		XMVECTOR vec = XMLoadFloat2(&_direction);
-		vec = XMVector2Normalize(vec);
+		vec = XMVector2Normalize(vec) * (1.0f - EPSILON);
 		XMStoreFloat2(&_direction, vec);
 	}
 }
