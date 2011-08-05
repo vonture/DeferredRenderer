7d4e957d0582a6487123f5a25e383a8e351cddb2
Geoff Lang <geoff@vonture.net>
2011-08-05 15:43:32 -0400

    Configuration pane now inherics from the table layout instead of the scroll control.

--- src/ConfigurationPaneBase.h
+++ src/ConfigurationPaneBase.h
@@ -2,18 +2,17 @@
 
 #include "Defines.h"
 #include "IUpdateable.h"
-#include "Gwen/Controls/ScrollControl.h"
+#include "Gwen/Controls/Layout/Table.h"
 
-class ConfigurationPaneBase : public Gwen::Controls::ScrollControl, public IUpdateable
+class ConfigurationPaneBase : public Gwen::Controls::Layout::Table, public IUpdateable
 {
 public:
 	ConfigurationPaneBase(Gwen::Controls::Base* parent, const WCHAR* name) 
-		: Gwen::Controls::ScrollControl(parent) 
+		: Gwen::Controls::Layout::Table(parent) 
 	{ 
 		SetName(Gwen::Utility::UnicodeToString(name));
-		SetScroll(false, true);
-		SetAutoHideBars(true);
+		SetColumnCount(1);
 	}
-	
+
 	virtual void OnFrameMove(double totalTime, float dt) { };
 };
