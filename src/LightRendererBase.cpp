133e60caeab14cdb28e7042d9879db1eab444f37
Geoff Lang <geoff@vonture.net>
2011-08-05 15:36:35 -0400

    Created a non-templated light renderer base class.

---
+++ src/LightRendererBase.cpp
@@ -0,0 +1,42 @@
+#include "LightRendererBase.h"
+
+HRESULT LightRendererBase::OnD3D11CreateDevice(ID3D11Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc)
+{
+	HRESULT hr;
+
+	V_RETURN(_dsStates.OnD3D11CreateDevice(pd3dDevice, pBackBufferSurfaceDesc));
+	V_RETURN(_samplerStates.OnD3D11CreateDevice(pd3dDevice, pBackBufferSurfaceDesc));
+	V_RETURN(_blendStates.OnD3D11CreateDevice(pd3dDevice, pBackBufferSurfaceDesc));
+	V_RETURN(_rasterStates.OnD3D11CreateDevice(pd3dDevice, pBackBufferSurfaceDesc));
+
+	return S_OK; 
+}
+
+void LightRendererBase::OnD3D11DestroyDevice()
+{
+	_dsStates.OnD3D11DestroyDevice();
+	_samplerStates.OnD3D11DestroyDevice();
+	_blendStates.OnD3D11DestroyDevice();
+	_rasterStates.OnD3D11DestroyDevice();
+}
+
+HRESULT LightRendererBase::OnD3D11ResizedSwapChain( ID3D11Device* pd3dDevice, IDXGISwapChain* pSwapChain,
+	const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc)
+{
+	HRESULT hr;
+
+	V_RETURN(_dsStates.OnD3D11ResizedSwapChain(pd3dDevice, pSwapChain, pBackBufferSurfaceDesc));
+	V_RETURN(_samplerStates.OnD3D11ResizedSwapChain(pd3dDevice, pSwapChain, pBackBufferSurfaceDesc));
+	V_RETURN(_blendStates.OnD3D11ResizedSwapChain(pd3dDevice, pSwapChain, pBackBufferSurfaceDesc));
+	V_RETURN(_rasterStates.OnD3D11ResizedSwapChain(pd3dDevice, pSwapChain, pBackBufferSurfaceDesc));
+
+	return S_OK;
+}
+
+void LightRendererBase::OnD3D11ReleasingSwapChain()
+{
+	_dsStates.OnD3D11ReleasingSwapChain();
+	_samplerStates.OnD3D11ReleasingSwapChain();
+	_blendStates.OnD3D11ReleasingSwapChain();
+	_rasterStates.OnD3D11ReleasingSwapChain();
+}
---
+++ src/LightRendererBase.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Defines.h"
+#include "IHasContent.h"
+#include "DeviceStates.h"
+#include "ModelInstance.h"
+#include "Camera.h"
+#include "GBuffer.h"
+
+class LightRendererBase : public IHasContent
+{
+private:	
+	DepthStencilStates _dsStates;
+	SamplerStates _samplerStates;
+	BlendStates _blendStates;
+	RasterizerStates _rasterStates;
+
+protected:
+	DepthStencilStates* GetDepthStencilStates() { return &_dsStates; }
+	SamplerStates* GetSamplerStates() { return &_samplerStates; }
+	BlendStates* GetBlendStates() { return &_blendStates; }
+	RasterizerStates* GetRasterizerStates() { return &_rasterStates; }
+
+	virtual UINT GetMaxShadowedLights() const = 0;
+
+public:
+	virtual HRESULT RenderShadowMaps(ID3D11DeviceContext* pd3dImmediateContext, std::vector<ModelInstance*>* models,
+		Camera* camera, AxisAlignedBox* sceneBounds) = 0;
+	virtual HRESULT RenderLights(ID3D11DeviceContext* pd3dImmediateContext, Camera* camera,
+		GBuffer* gBuffer) = 0;
+
+	virtual UINT GetCount() = 0;
+	virtual UINT GetCount(bool shadowed) = 0;
+
+	virtual void Clear() = 0;
+
+	virtual HRESULT OnD3D11CreateDevice(ID3D11Device* pd3dDevice, 
+		const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc);
+	virtual void OnD3D11DestroyDevice();
+
+	virtual HRESULT OnD3D11ResizedSwapChain( ID3D11Device* pd3dDevice, IDXGISwapChain* pSwapChain,
+		const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc);
+	virtual void OnD3D11ReleasingSwapChain();
+};
