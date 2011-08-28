98fdbc4dcf7e92d31a99e33d9e2a01eacab678b0
Geoff Lang <geoff@vonture.net>
2011-08-28 14:27:07 -0400

    Removed the now unneeded shader loading function.

--- src/deferred-renderer/ShaderLoader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "PCH.h"
-
-// TO BE REMOVED
-HRESULT CompileShaderFromFile(WCHAR* szFileName,  LPCSTR szEntryPoint, LPCSTR szShaderModel, 
-	D3D_SHADER_MACRO* defines, ID3DBlob** ppBlobOut);
