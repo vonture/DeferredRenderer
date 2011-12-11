ebed4905dd0ff24593a392c2f99a184d02e016da
Geoff Lang <geoff@vonture.net>
2011-12-10 21:37:56 -0500

    Added a gwen control for displaying srv textures.

---
+++ src/deferred-renderer/LiveTextureControl.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "PCH.h"
+#include "Gwen/Controls/Base.h"
+#include "Gwen/Texture.h"
+
+class LiveTextureControl : public Gwen::Controls::Base
+{
+private:
+	Gwen::Texture _tex;
+
+protected:
+	virtual void Render(Gwen::Skin::Base* skin);
+
+public:
+	LiveTextureControl(Gwen::Controls::Base* parent);
+
+	void SetTexture(ID3D11ShaderResourceView* srv);
+};
