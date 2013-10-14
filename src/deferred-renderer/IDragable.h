75cfd86ce9e301a309f35644c331804e2b009234
Geoff Lang <geoff@vonture.net>
2013-10-14 16:51:34 -0400

    Removed the requirement for dragables to have a scale.

--- src/deferred-renderer/IDragable.h
+++ src/deferred-renderer/IDragable.h
@@ -10,8 +10,8 @@ public:
 	virtual const XMFLOAT3& GetPosition() const = 0;
 	virtual void SetPosition(const XMFLOAT3& pos) = 0;
 
-	virtual float GetScale() const = 0;
-	virtual void SetScale(float scale) = 0;
+	//virtual float GetScale() const = 0;
+	//virtual void SetScale(float scale) = 0;
 
 	virtual void FillBoundingObjectSet(BoundingObjectSet* set) = 0;
 	virtual bool RayIntersect(const Ray& ray, float* dist) = 0;
